0ab5fdd7aeb913a0303cdc5e2170927d4e9081d4
nikohonu <nikohonu@proton.me>
2024-04-25 17:23:07 +0300

    refactor level_selection.gd

--- scripts/level_selection/level_selection.gd
+++ scripts/level_selection/level_selection.gd
@@ -1,15 +1,17 @@
-extends Control # need refectoring 
+extends Control
+
 
 var overwrite_level: int
 
-@onready var level_buttons = $MarginContainer/VBoxContainer/GridContainer.get_children()
+@export var level_button_container: GridContainer
+
+@onready var level_buttons = level_button_container.get_children()
 
 
 func _ready():
 	if Global.saved_level != -1:
 		level_buttons[Global.saved_level].has_save = true
-	var level_count = len(Global.levels)
-	for i in range(level_count):
+	for i in range(Global.LEVEL_COUNT):
 		level_buttons[i].icon = _level_to_kanji(i)
 		if i < Global.progress:
 			level_buttons[i].status = LevelButton.Status.COMPLETED
@@ -24,31 +26,19 @@ func _ready():
 func _level_to_kanji(level: int):
 	if level < 12:
 		@warning_ignore("integer_division")
-		var j = level / 3
-		if j == 0:
-			return "春"
-		if j == 1:
-			return "夏"
-		if j == 2:
-			return "秋"
-		if j == 3:
-			return "冬"
-	if level == 12:
-		return "刀"
-	if level == 13:
-		return "昏"
-	if level == 14:
-		return "侍"
-	if level == 15:
-		return "将"
-	if level == 16:
-		return "桜"
-	if level == 17:
-		return "死"
-
-
-func _on_back_pressed():
-	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+		var value = level / 3
+		match value:
+			0: return "春"
+			1: return "夏"
+			2: return "秋"
+			3: return "冬"
+	match level:
+		12: return "刀"
+		13: return "昏"
+		14: return "侍"
+		15: return "将"
+		16: return "桜"
+		17: return "死"
 
 
 func _start_level(level: int):
@@ -61,6 +51,10 @@ func _start_level(level: int):
 		get_tree().change_scene_to_file("res://scenes/game/game.tscn")
 	else:
 		$PanelContainer.visible = true
+
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 
 func _on_ok_pressed():
