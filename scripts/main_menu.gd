a5ebb82be561ab577b78d3f4c6945f004183cb3f
nikohonu <nikohonu@proton.me>
2024-04-25 17:25:57 +0300

    refactor main_menu.gd

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,12 +1,8 @@
-extends Control # need refectoring 
+extends Control
 
 
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://scenes/level_selection/level_selection.tscn")
-
-
-func _on_exit_pressed():
-	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
 
 
 func _on_control_pressed():
@@ -15,3 +11,7 @@ func _on_control_pressed():
 
 func _on_custom_pressed():
 	get_tree().change_scene_to_file("res://scenes/custom.tscn")
+
+
+func _on_exit_pressed():
+	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
